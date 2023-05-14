d1e427a8d2ebf6c8fa9ebaa4171225dbecd1e4e3
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-14 00:16:45 -0400

    Add Calculator UI

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let a = 3;
-let b = 3; 
-let op = 0;
 function add(a, b){
     return a + b;
 }
@@ -17,9 +14,29 @@ function divide(a, b){
     return a / b;
 }
 
+function operate(a, b, op){
+    if(op == 0){
+        return add(a,b);
+    }
+    else if(op == 1){
+        return subtract(a,b);
+    }
+    else if(op == 2){
+        return multiply(a,b);
+    }
+    else{
+        return divide(a, b);
+    }
+}
+
+/*TEST
 let arr = [3, 3, 8, 35];
+let a = 3;
+let b = 3; 
+let op = 0;
 
 console.log("Add: " + add(a, b));
 console.log("Subtract: " + subtract(a,b));
 console.log("Multiply: " + multiply(a,b));
 console.log("Divide: " + divide(a,b));
+*/
